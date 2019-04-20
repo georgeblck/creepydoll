1c60fa1772b71e5b6305b46c3abeef056733bdff
Nikolai Huckle <n.huckle@posteo.de>
2019-04-20 16:30:25 +0200

    Added playback video

--- letloose.py
+++ letloose.py
@@ -139,6 +139,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
                     play_mp3(chosenSound)
                 camera.stop_recording()
                 camera.stop_preview()
+                syscmd("omxplayer " + destination)
                 # update the last uploaded timestamp and reset the motion
                 # counter
                 lastUploaded = timestamp
