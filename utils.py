160c59e05f4cd57a492ac0c747254b41b9735a5d
Nikolai Huckle <n.huckle@posteo.de>
2019-04-27 06:16:41 +0200

    Bug searching

--- utils.py
+++ utils.py
@@ -34,11 +34,11 @@ def syscmd(cmd, waiting=True):
         p.wait()
 
 
-def record_wav(length, filename):
+def record_wav(length, filename, chan, rat, chk):
     FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 22000
-    CHUNK = 1024
+    CHANNELS = chan#1
+    RATE = rat#22000
+    CHUNK = chk#1024
     RECORD_SECONDS = length
     WAVE_OUTPUT_FILENAME = "recordings/" + filename + ".wav"
     audio = pyaudio.PyAudio()
